b033571bdaac7f57e3557ef6f2028b7f52df5e08
Spencer Johnson <sljohnson2222@gmail.com>
2022-05-28 10:11:11 -0500

    db working now

--- db/migrate/20220528134519_create_cart_items.rb
+++ db/migrate/20220528134519_create_cart_items.rb
@@ -2,11 +2,9 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
       t.string :cart_id
-      t.string :integer
-      t.string :price
-      t.string :integer
-      t.string :db_id
-      t.string :integer
+      t.integer :db_id
+      t.float :price
+      t.string :title
 
       t.timestamps
     end
